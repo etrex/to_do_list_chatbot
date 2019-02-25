492b2b113f961f7fce35a560c461c69a6a77d686
etrex kuo <et284vu065k3@gmail.com>
2019-02-25 10:04:23 +0800

    add empty message for index page

--- app/helpers/line_helper.rb
+++ app/helpers/line_helper.rb
@@ -1,10 +1,10 @@
 module LineHelper
-  def flex_list_view(title: "title", menu_action: nil, new_action: nil, list: [])
+  def flex_list_view(title: "title", menu_action: nil, new_action: nil, list: [], empty_message: "no data")
     flex_bubble(
       body: flex_vertical_box([
         flex_nav(title: title, menu_action: menu_action, new_action: new_action),
         flex_separator,
-        flex_list_body(list, margin: "lg"),
+        flex_list_body(list, margin: "lg") || flex_empty_body(empty_message),
       ])
     )
   end
@@ -12,11 +12,16 @@ module LineHelper
   private
 
   def flex_list_body(list, **params)
+    return nil if list.empty?
     list = list.map do |data|
       action = flex_message_action("/#{data.class.to_s.downcase.pluralize}/#{data.id}")
       flex_horizontal_box([flex_text(data.name)], action: action)
     end
     flex_vertical_box([list], **params)
+  end
+
+  def flex_empty_body(text, **params)
+    flex_text(text, **params)
   end
 
   def flex_nav(title:, menu_action: nil, new_action: nil)
