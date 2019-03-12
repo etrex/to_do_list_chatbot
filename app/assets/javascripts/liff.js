c27bbe0bac815d169b6b577d6a9b66434072a120
etrex kuo <et284vu065k3@gmail.com>
2019-03-13 02:35:53 +0800

    remove es6

--- app/assets/javascripts/liff.js
+++ app/assets/javascripts/liff.js
@@ -2,6 +2,6 @@ window.addEventListener("liff_submit", function(event){
   var json = JSON.stringify(event.detail.data);
   var url = event.detail.url;
   var method = event.detail.method;
-  var request_text = `${method} ${url}\n${json}`;
+  var request_text = method + " " + url + "\n" + json;
   liff_send_text_message(request_text);
 });
